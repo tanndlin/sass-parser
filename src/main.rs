17077c38dae25b465fbf18c78e499f37e7197c74
tanndlin <tanndlin@gmail.com>
2024-09-18 18:53:40 -0400

    Remove prints

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,12 @@ mod types;
 mod utils;
 
 fn main() {
-    let input = utils::read_file("styles.scss");
+    let input = utils::read_file("stress.scss");
     let mut l = lexer::Lexer::new(input);
     let tokens = l.get_tokens();
-    println!("{:?}", tokens);
 
     let mut parser = parse::Parser::new(tokens);
     let classes = parser.parse();
-    println!("{:?}", classes);
 
     let compiler = compiler::Compiler::new(classes);
     compiler.compile("styles.css");
