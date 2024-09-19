43ccd2961e9e4f3c197c4776cf17b7a33c575995
tanndlin <tanndlin@gmail.com>
2024-09-19 16:24:41 -0400

    Take file name as CLI arg

--- compare_time.ps1
+++ compare_time.ps1
@@ -1,4 +1,4 @@
 just build
 
-Measure-Command { sass stress.scss }
-Measure-Command { target/release/sass-parser.exe }
+Measure-Command { sass stress.scss > stress.css}
+Measure-Command { target/release/sass-parser.exe stress.scss}
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,22 @@ mod types;
 mod utils;
 
 fn main() {
-    let input = utils::read_file("stress.scss");
+    // Take filename from args
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <filename>", args[0]);
+        std::process::exit(1);
+    }
+
+    let file_name = &args[1];
+    if !file_name.ends_with(".scss") {
+        eprintln!("Expected a .scss file, got {}", file_name);
+        std::process::exit(1);
+    }
+
+    let result_file_name = file_name.replace(".scss", ".css");
+
+    let input = utils::read_file(file_name);
     let mut l = lexer::Lexer::new(input);
     let tokens = l.get_tokens();
 
@@ -13,5 +28,5 @@ fn main() {
     let classes = parser.parse();
 
     let compiler = compiler::Compiler::new(classes);
-    compiler.compile("styles.css");
+    compiler.compile(&result_file_name);
 }
